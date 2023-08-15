ed861013943d1bcf7cdeee74721c4523d10ef56a
Jörgen <jorgen.smas@trustly.com>
2023-08-15 22:10:56 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -Dlogback.configurationFile=src/main/resources/logback-file.xml -cp build/libs/com.example.ktor-sample-0.0.1.jar:$(find build/install | tr '\n' ':') se.smasseman.kanonen.web.ApplicationKt
+sudo java -Dstaticfiles=./src/main/resources/static -Dport=80 -Dpins=. -Dsequences=sequences -Dlogback.configurationFile=src/main/resources/logback-file.xml -cp build/libs/com.example.ktor-sample-0.0.1.jar:$(find build/install | tr '\n' ':') se.smasseman.kanonen.web.ApplicationKt
